05c2dd7b2e9b7d85aacf65cf4db51b3b51b2b228
Hays Stanford <stanfordhays@gmail.com>
2016-08-14 19:59:54 -0500

    finish button now appears once all questions are answered

--- app/components/QuestionList.jsx
+++ app/components/QuestionList.jsx
@@ -3,6 +3,11 @@ var Question = require("./Question.jsx");
 
 module.exports = React.createClass({
 	render:function() {
+		var finishButton;
+		if(this.props.questions.length == 0) {
+			finishButton = <button type="button" className="btn btn-primary btn-lg btn-block">Finish</button>
+		}
+
 		return(
 			<div className="row">
 				<div className="col-lg-2">
@@ -15,6 +20,10 @@ module.exports = React.createClass({
 								<Question info={q} key={"question"+q.questionNum} />
 							)
 						})
+					}
+					{
+						//Will not be included if finishButton is left undefined by previous if statement
+						finishButton
 					}
 				</div>
 			</div>
